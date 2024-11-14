a7a71b00ace5b0b587d5d333f2f5742080d772cf
georgeagg <cs05172@uoi.gr>
2024-11-14 17:02:31 +0200

    Completed lab05!

--- lab05.s
+++ lab05.s
@@ -68,6 +68,11 @@ taken:
 # TODO: Add an example where an instruction passes its result to the 2nd following instruction
 # There should be no stalls
 # ----------------------------------------------------------------------------------------
+
+    add  t1, s0, s1   # t1 = 1
+    addi t2, s0, s2   # t2 = 2
+    add  t3, t1, s3   # t3 = 4
+
     # nop instructions added between examples
     add  zero, zero, zero  
     add  zero, zero, zero  
@@ -79,6 +84,11 @@ taken:
 #  registers of both of the two instructions preceeding it. It should get the newest value.
 # There should be no stalls
 # ----------------------------------------------------------------------------------------
+
+    add  t1, s0, s1   # t1 = 1
+    addi t1, s0, s2   # t1 = 2
+    add  t3, t1, s3   # t3 = 5
+
     # nop instructions added between examples
     add  zero, zero, zero  
     add  zero, zero, zero  
@@ -88,6 +98,9 @@ taken:
 # TODO: Add an example with a load stalling for 1 cycle to pass a value to a NOT-TAKEN branch 
 #  Is this a data hazard or a control hazard?
 # ----------------------------------------------------------------------------------------
+    lw   t3, 4(a0)
+    beq  t3, zero, exit   
+
     # nop instructions added between examples
     add  zero, zero, zero  
     add  zero, zero, zero  
@@ -96,6 +109,11 @@ taken:
 # ----------------------------------------------------------------------------------------
 # TODO: Add an example with taken branch to a label which is immediately following the branch
 # ----------------------------------------------------------------------------------------
+
+beq  zero, s0, nextInstr
+nextInstr:
+    add  t0, s1, s2 
+    add  t1, s2, s3 
 
 
 
